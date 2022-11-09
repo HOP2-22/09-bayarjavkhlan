200dbfb7dcae7ff238f62e95aa532beaa8e3fb4f
Jawhaa1224 <jawkhlan626@gmail.com>
2022-11-09 19:09:38 +0800

    aaa

--- Block2_with_tailwindcss/src/Pages/RouterChallange/DataBody.jsx
+++ Block2_with_tailwindcss/src/Pages/RouterChallange/DataBody.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { ColorModeContext } from "./ThemeContext";
+import { useContext } from "react";
 
 const DataBody = ({ title, comment, avatar, image, name, date }) => {
+  const { theme, changeTheme } = useContext(ColorModeContext);
+
   return (
-    <div className="max-w-[370px] max-h-[440px] overflow-hidden rounded-2xl flex flex-col items-center bg-white">
+    <div
+      className={`max-w-[370px] max-h-[440px] overflow-hidden rounded-2xl flex flex-col items-center ${
+        theme ? "bg-white" : "bg-black"
+      }`}
+    >
       <div
         className="h-[147px] bg-cover"
         style={{
