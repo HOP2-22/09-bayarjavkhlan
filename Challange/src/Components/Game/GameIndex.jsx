b6eb086b093296e6d7d324e4672ab4ac8e66072d
Jawhaa1224 <jawkhlan626@gmail.com>
2022-11-30 20:45:48 +0800

    bvvr dussan

--- Challange/src/Components/Game/GameIndex.jsx
+++ Challange/src/Components/Game/GameIndex.jsx
@@ -3,22 +3,27 @@ import top from "../../IMG/UpperBack.png";
 import bottom from "../../IMG/LowerBack.png";
 import target from "../../IMG/Rectangle.png";
 import hammer from "../../IMG/hammer.png";
+import History from "./History";
 
 const GameIndex = () => {
   const [click, setClick] = useState(false);
   const [score, setScore] = useState(0);
+  const [total, setTotal] = useState(30);
+  const [putTotal, setPutTotal] = useState(30);
   const [mouseClick, setMouseClick] = useState(false);
   const [time, setTime] = useState(30);
   const [y, setY] = useState();
   const [x, setX] = useState();
-  const [move, setMove] = useState(new Array(15).fill(false));
+  const [move, setMove] = useState([]);
   const translate = () => {
-    let number = Math.floor(Math.random() * 15);
-    let newMove = new Array(15).fill(false);
+    let number = Math.floor(Math.random() * putTotal);
+    let newMove = new Array(Number(putTotal)).fill(false);
     newMove[number] = true;
     setMove(newMove);
   };
-
+  useEffect(() => {
+    setMove(new Array(Number(putTotal)).fill(false));
+  }, [putTotal]);
   useEffect(() => {
     const move = (e) => {
       setX(e.clientX);
@@ -38,6 +43,7 @@ const GameIndex = () => {
       document.removeEventListener("click", click);
     };
   }, []);
+
   useEffect(() => {
     if (time === 0) {
       setClick(false);
@@ -51,7 +57,7 @@ const GameIndex = () => {
     }
 
     return () => clearInterval(interval);
-  }, [time, click]);
+  }, [click]);
   return (
     <>
       <div className="relative w-full h-screen bg-[#b40001] flex flex-col gap-14 items-center justify-center">
@@ -61,14 +67,41 @@ const GameIndex = () => {
             <div className="pl-2">{time}</div>
           </div>
           <div className="">Score:{score}</div>
+
+          <div className="flex gap-[43px]">
+            <p>Total:</p>
+            <input
+              className="text-green-300 w-20"
+              value={total}
+              onChange={(event) => {
+                const result = event.target.value.replace(/\D/g, "");
+                setTotal(result);
+              }}
+              onKeyDown={(el) => {
+                if (el.code === "Enter") {
+                  setPutTotal(total);
+                }
+              }}
+            />
+          </div>
         </div>
-        <div className="grid grid-cols-5">
+        <div
+          className={`${
+            putTotal < 20
+              ? "grid-cols-5"
+              : putTotal < 25
+              ? "grid-cols-6"
+              : putTotal < 35
+              ? "grid-cols-7"
+              : putTotal < 41 && "grid-cols-8"
+          } grid`}
+        >
           {move.map((boolen, index) => (
             <div
-              className="relative flex flex-col items-center justify-center pt-10"
+              className="relative flex flex-col items-center justify-center"
               key={index}
             >
-              <img src={top} alt="" className="w-0.4" />
+              <img src={top} alt="" draggable="false" className="w-0.4" />
               <img
                 src={target}
                 alt=""
@@ -80,21 +113,28 @@ const GameIndex = () => {
                 onClick={() => {
                   setScore((prev) => prev + 1);
                 }}
+                draggable="false"
               />
-              <img src={bottom} alt="" className="w-0.4 -translate-y-3" />
+              <img
+                src={bottom}
+                alt=""
+                className="w-0.4 -translate-y-3"
+                draggable="false"
+              />
             </div>
           ))}
         </div>
-        <div className="py-40">
+        <div className="py-10 flex justify-between">
+          <div></div>
           <button
             className="bg-teal-accent-400 px-24 py-7 rounded-full text-white cursor-pointer active:bg-teal-accent-700"
             onClick={() => {
               setClick(!click);
-              console.log(mouseClick);
             }}
           >
             START
           </button>
+          <History score={score} />
         </div>
       </div>
       <div style={{ position: "absolute", top: y - 60, left: x + 30 }}>
@@ -113,7 +153,7 @@ const GameIndex = () => {
       >
         <div className="w-[300px] h-[400px] bg-white p-10 flex flex-col justify-between items-center">
           <div className="font-semibold text-3xl">
-            {score > 20 ? "Victory" : "Defeat"}
+            {score >= 20 ? "Victory" : "Defeat"}
           </div>
           <div className="pb-20">
             <p className="font-medium text-xl">Your score is {score}</p>
@@ -129,7 +169,7 @@ const GameIndex = () => {
             onClick={() => {
               setTime(30);
               setScore(0);
-              setMove(new Array(15).fill(false));
+              setMove(new Array(Number(putTotal)).fill(false));
             }}
             className="py-3 px-7 bg-green-600 rounded-3xl"
           >
